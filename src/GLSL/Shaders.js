c9a2cad1bb9c8ea892c688fee3401ae8ba56672f
Nodejs Puerto Rico <nodejs787@gmail.com>
2017-06-12 14:14:36 +0000

    BIG RAINBOW

--- src/GLSL/Shaders.js
+++ src/GLSL/Shaders.js
@@ -1,19 +1,21 @@
 const Shaders = {
 vertex:
-`attribute vec2 a_position;
-//attribute vec4 a_color;
-//varying vec4 v_color;
+`attribute vec3 a_color;
+attribute  vec2 a_position;
+varying    vec3 v_color;
+
 void main()
 {
+  v_color = a_color;
   gl_Position = vec4(a_position, 0.0, 1.0);
-  //v_color = a_color;
 }`,
 fragment:
 `precision mediump float;
-//varying vec4 v_color;
+varying    vec3 v_color;
+
 void main()
 {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(v_color,1.0);
 }`
 }
 export default Shaders
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -14,95 +14,15 @@ class Canvas extends React.Component {
   
   
   // // WebGL configuration
-  // initBuffers(gl){
-  //   const triangleVertices = new Float32Array([
-  //   // X    Y    Z      R   G   B
-  //     -1.0,+1.0,+0.0,   0.0,0.0,0.0,
-  //     -1.0,+0.0,+0.0,   0.3,1.0,0.5,
-  //     +1.0,-1.0,+0.0,   0.6,0.2,1.0
-  //   ])
-  //   const posOffset = triangleVertices.BYTES_PER_ELEMENT * 3
-  //   const vertexStride = triangleVertices.BYTES_PER_ELEMENT * 6
-  //   gl.vertexAttribPointer(
-  //     0,
-  //     3,
-  //     gl.FLOAT,
-  //     false,
-  //     vertexStride,
-  //     0
-  //     )
-  //     gl.vertexAttribPointer(
-  //     0,
-  //     3,
-  //     gl.FLOAT,
-  //     false,
-  //     vertexStride,
-  //     posOffset
-  //     )
-  //   const triangleVertexBuffer = gl.createBuffer()
-  //   gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer)
-  //   gl.bufferData(gl.ARRAY_BUFFER, triangleVertices, gl.STATIC_DRAW)
-    
-    
-  //   console.log(gl, "<-- GL context", triangleVertices)
-  // }
-  // initShadders(gl){
-  //   const vertexShader = Shaders.vertex
-  //   const fragmentShader = Shaders.fragment
-  //   const vertex = gl.createShader(gl.VERTEX_SHADER)
-  //   const fragment = gl.createShader(gl.FRAGMENT_SHADER)
-  //   gl.shaderSource(vertex, vertexShader)
-  //   gl.shaderSource(fragment, fragmentShader)
-  //   gl.compileShader(vertex)
-  //   if(!gl.getShaderParameter(vertex, gl.COMPILE_STATUS)){
-  //     console.error("ERROR:", gl.getShaderInfoLog(vertex))
-  //     return
-  //   }
-  //   gl.compileShader(fragment)
-  //   if(!gl.getShaderParameter(fragment, gl.COMPILE_STATUS)){
-  //     console.error("ERROR:", gl.getShaderInfoLog(fragment))
-  //     return
-  //   }
-  //   const program = gl.createProgram()
-  //   gl.attachShader(program, vertex)
-  //   gl.attachShader(program, fragment)
-  //   gl.linkProgram(program)
-  //   if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
-  //     console.error("LINK PROBLEM", gl.getProgramInfoLog(program))
-  //     return
-  //   }
-  //   gl.validateProgram(program)
-  //   if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS)){
-  //     console.error("VALIDATION ERROR", gl.getProgramInfoLog(program))
-  //     return
-  //   }
-  //   const positionAttrLocation = gl.getAttribLocation(program, 'vertPosition')
-  //   const colorAttrLocation = gl.getAttribLocation(program, 'vertColor')
-    
-  //   gl.enableVertexAttribArray(positionAttrLocation)
-  //   gl.enableVertexAttribArray(colorAttrLocation)
-
-
-  
-  //   console.log(Shaders.vertex)
-  //   return program
-    
-  // }
-  
-  // initScene(gl){
-  //   this.initBuffers(gl)
-  //   const program = this.initShadders(gl)
-  //   return program
-  // }
-  
-  // drawScene(gl, program){
-    
-  //   gl.useProgram(program)
-  //   gl.drawArrays(gl.TRIANGLES, 0, 3)
-    
-  //   console.log("DRAW", program)
-  // }
-  // // WebGL end config
+    initWebGl(gl, width, height){
+    gl.viewport(0,0, width, height)
+    gl.clearColor(0,0,0,1)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    gl.enable(gl.DEPTH_TEST)
+    gl.depthFunc(gl.LEQUAL)
+    console.log("WebGl Initialized.")
+  }
+  // Shadders
   initShadders(gl){
     const vertex = gl.createShader(gl.VERTEX_SHADER)
     const fragment = gl.createShader(gl.FRAGMENT_SHADER)
@@ -129,14 +49,7 @@ class Canvas extends React.Component {
     console.log("Shaders Initialized.")
     return program
   }
-  initWebGl(gl, width, height){
-    gl.viewport(0,0, width, height)
-    gl.clearColor(0,0,0,1)
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    gl.enable(gl.DEPTH_TEST)
-    gl.depthFunc(gl.LEQUAL)
-    console.log("WebGl Initialized.")
-  }
+  // Buffers
   initBuffers(gl, program){
     // const triangleVertices = new Float32Array([
     // // X    Y    Z      R   G   B   A
@@ -144,23 +57,25 @@ class Canvas extends React.Component {
     //   -0.5,-0.5,+0.0,   0.0,1.0,0.0,1.0,
     //   +0.5,-0.5,+0.0,   0.0,0.0,1.0,1.0
     // ])
-    const triangleVertices = new Float32Array([
+    const triangleVertices = [
     // X    Y      R   G   B   
-      +0.0,+0.5,   //1.0,0.0,0.0,
-      -0.5,-0.5,   //0.0,1.0,0.0,
-      +0.5,-0.5    //0.0,0.0,1.0
-    ])
-    const positionAttrLocation = gl.getAttribLocation(program, 'a_position')
-    const colorAttrLocation = gl.getAttribLocation(program, 'a_color')
-    console.log(positionAttrLocation  , colorAttrLocation)
-    const vertexStride = Float32Array.BYTES_PER_ELEMENT * 2
+      +0.0,+1.0,   1.0,0.0,0.0,
+      -1.0,-1.0,   0.0,1.0,0.0,
+      +1.0,-1.0,   0.0,0.0,1.0
+    ]
+    
+   
+    const vertexStride = Float32Array.BYTES_PER_ELEMENT * 5
     const triangleVertexBuffer = gl.createBuffer()
     if(!triangleVertexBuffer) {
       console.log("Invalid Vertex Buffer")
       return false
     }
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, triangleVertices, gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW)
+    const positionAttrLocation = gl.getAttribLocation(program, 'a_position')
+    const colorAttrLocation = gl.getAttribLocation(program, 'a_color')
+    console.log(positionAttrLocation  , colorAttrLocation)
     gl.vertexAttribPointer(
       positionAttrLocation,
       2,
@@ -169,16 +84,29 @@ class Canvas extends React.Component {
       vertexStride,
       0
     )
+    gl.vertexAttribPointer(
+      colorAttrLocation,
+      3,
+      gl.FLOAT,
+      gl.FALSE,
+      vertexStride,
+      2 * Float32Array.BYTES_PER_ELEMENT
+    )
     gl.enableVertexAttribArray(positionAttrLocation)
+    gl.enableVertexAttribArray(colorAttrLocation)
+
 
     console.log("Buffers Initialized.")
   }
+  // Render (draw)
   draw(gl, program){
     gl.clearColor(0,0,0,1)
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
     gl.useProgram(program)
     gl.drawArrays(gl.TRIANGLES, 0, 3)
   }
+  
+  //////// WebGL end configuration
   
   updateCanvas() {
     const canvas = this.refs.canvas
